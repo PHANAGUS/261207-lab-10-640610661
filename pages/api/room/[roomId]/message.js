aa9cb4ed87ed0d4bacad1e5f63da42e176dcd110
PHANAGUS <phanarin03072002@gmail.com>
2022-09-22 17:51:15 +0700

    lab-10 update

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,13 +4,45 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+
+    const roomId = req.query.roomId;
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+
+    const result = rooms[roomIdx].messages;
+    return res.json({ ok: true, massages: result });
   } else if (req.method === "POST") {
     const rooms = readDB();
 
+    const roomId = req.query.roomId;
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+
     //read request body
     const text = req.body.text;
+    if (typeof text !== "string")
+      return res.status(400).json({ ok: false, message: "Invalid text input" });
 
     //create new id
     const newId = uuidv4();
+    const newMassage = [
+      {
+        messageId: newId,
+        text: text,
+      },
+    ];
+    // const newMs = rooms[roomIdx].messages;
+    // newMs.push(newMassage);
+    // rooms[roomIdx].messages = newMs;
+
+    rooms[roomIdx].messages.push(newMassage);
+    rooms.push({ text: "hi" });
+
+    writeDB(rooms);
+    // return res.json({ ok: true, massage: newMassage });
+    // console.log(rooms[roomIdx].messages);
+    return res.json({ ok: true, rooms });
   }
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -1,7 +1,24 @@
 import { writeDB, readDB } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
+  const rooms = readDB();
+
   //read value from URL
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
+
+  const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+  if (roomIdx === -1)
+    return res.status(404).json({ ok: false, message: "Invalid room id" });
+
+  const msgIdx = rooms[roomIdx].messages.findIndex(
+    (x) => x.messageId === messageId
+  );
+  if (msgIdx === -1)
+    return res.status(404).json({ ok: false, massage: "Invalid massage id" });
+
+  rooms[roomIdx].messages.splice(msgIdx, 1);
+  writeDB(rooms);
+
+  return res.json({ ok: true });
 }
